bcf7ff8fbae81a430e290229de762def4d71a31d
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-11-12 19:44:54 -0600

    Edit Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,368 +1,172 @@
 import {
-    useColorModeValue,
-    useDisclosure,
-    Icon,
-    chakra,
-    VStack,
+    Box,
     Button,
+    Link,
+    Heading,
+    chakra,
+    CloseButton,
     Flex,
     HStack,
     IconButton,
-    Box,
-    Text,
-    Link,
+    useColorModeValue,
+    useDisclosure,
+    VStack,
+    Divider,
     Image,
     useColorMode,
 } from "@chakra-ui/react"
-import { useViewportScroll } from "framer-motion"
 import React from "react"
+import { IoLanguage, IoMoon, IoSunny } from "react-icons/io5"
 
-import { AiOutlineMenu } from "react-icons/ai"
-import { IoIosArrowDown } from "react-icons/io"
-import { FaDiscord, FaTimes, FaTwitter } from "react-icons/fa"
-import { motion } from "framer-motion"
-
-function Navbar() {
-    const ref = React.useRef(null)
-    const [y, setY] = React.useState(0)
-    //@ts-ignore
-    const height = ref.current ? ref.current.getBoundingClientRect() : 0
-    const { scrollY } = useViewportScroll()
-    React.useEffect(() => {
-        return scrollY.onChange(() => setY(scrollY.get()))
-    }, [scrollY])
-    const cl = useColorModeValue("gray.800", "white")
+const Navbar = () => {
+    const bg = useColorModeValue("white", "gray.800")
+    const navBg = useColorModeValue("white", "gray.900")
     const mobileNav = useDisclosure()
 
-    //check if scrollY is greater than 0
-    const isScrolled = y > 0
     const { colorMode, toggleColorMode } = useColorMode()
 
-    const MobileNavContent = (
-        <VStack
-            position="absolute"
-            top={20}
-            mt={-2}
-            left={0}
-            right={0}
-            display={mobileNav.isOpen ? "flex" : "none"}
-            flexDirection="column"
-            pb={4}
-            bg="linear-gradient(359.86deg, rgba(0, 5, 5, 0.8) 75.56%, rgba(0, 5, 5, 0) 131.78%)"
-            backdropFilter="blur(19.5px)"
-            sx={{
-                borderImage:
-                    "linear-gradient(90deg, rgba(1, 218, 124, 0) 2.69%, rgba(175, 174, 184, 0) 2.7%, rgba(175, 174, 184, 0.21) 50.12%, rgba(175, 174, 184, 0) 96.58%)",
-                borderImageSlice: 1,
-                borderTopWidth: "1.5px",
-                borderTopStyle: "solid",
-                borderTopColor: "transparent",
-            }}
-            rounded="sm"
-            shadow="sm"
-            zIndex="999"
-            //animation
-            as={motion.div}
-            initial={{ opacity: 0, y: -20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition="0.3s linear"
-        >
-            <Box
-                w="full"
-                border="1.5px "
-                sx={{
-                    borderImage:
-                        "linear-gradient(90deg, rgba(175, 174, 184, 0.21) 2.69%, rgba(175, 174, 184, 0) 96.58%)",
-                    borderImageSlice: 1,
-                    borderBottomWidth: "1.5px",
-                    borderBottomStyle: "solid",
-                    borderBottomColor: "transparent",
-                }}
-                p={3}
-            >
-                <Text fontSize="xl" fontWeight="medium" ml={5}>
-                    Protocol
-                </Text>
-            </Box>
-            <Box
-                w="full"
-                border="1.5px "
-                sx={{
-                    borderImage:
-                        "linear-gradient(90deg, rgba(175, 174, 184, 0.21) 2.69%, rgba(175, 174, 184, 0) 96.58%)",
-                    borderImageSlice: 1,
-                    borderBottomWidth: "1.5px",
-                    borderBottomStyle: "solid",
-                    borderBottomColor: "transparent",
-                }}
-                p={3}
-            >
-                <Text fontSize="xl" fontWeight="medium" ml={5}>
-                    Governance
-                </Text>
-            </Box>
-            <Box
-                w="full"
-                sx={{
-                    borderImage:
-                        "linear-gradient(90deg, rgba(175, 174, 184, 0.21) 2.69%, rgba(175, 174, 184, 0) 96.58%)",
-                    borderImageSlice: 1,
-                    borderBottomWidth: "1.5px",
-                    borderBottomStyle: "solid",
-                    borderBottomColor: "transparent",
-                }}
-                p={3}
-            >
-                <Text fontSize="xl" fontWeight="medium" ml={5}>
-                    Docs
-                </Text>
-            </Box>
-            <Box
-                w="full"
-                border="1.5px "
-                sx={{
-                    borderImage:
-                        "linear-gradient(90deg, rgba(175, 174, 184, 0.21) 2.69%, rgba(175, 174, 184, 0) 96.58%)",
-                    borderImageSlice: 1,
-                    borderBottomWidth: "1.5px",
-                    borderBottomStyle: "solid",
-                    borderBottomColor: "transparent",
-                }}
-                p={3}
-            >
-                <Text fontSize="xl" fontWeight="medium" ml={5}>
-                    About
-                </Text>
-            </Box>
-            <Box
-                w="full"
-                border="1.5px "
-                sx={{
-                    borderImage:
-                        "linear-gradient(90deg, rgba(175, 174, 184, 0.21) 2.69%, rgba(175, 174, 184, 0) 96.58%)",
-                    borderImageSlice: 1,
-                    borderBottomWidth: "1.5px",
-                    borderBottomStyle: "solid",
-                    borderBottomColor: "transparent",
-                }}
-                p={3}
-            >
-                <Text fontSize="xl" fontWeight="medium" ml={5}>
-                    Twitter
-                </Text>
-            </Box>
-            <Box
-                w="full"
-                border="1.5px"
-                sx={{
-                    borderImage:
-                        "linear-gradient(90deg, rgba(175, 174, 184, 0.21) 2.69%, rgba(175, 174, 184, 0) 96.58%)",
-                    borderImageSlice: 1,
-                    borderBottomWidth: "1.5px",
-                    borderBottomStyle: "solid",
-                    borderBottomColor: "transparent",
-                }}
-                p={3}
-            >
-                <Text fontSize="xl" fontWeight="medium" ml={5}>
-                    Discord
-                </Text>
-            </Box>
-        </VStack>
-    )
-    return (
-        <chakra.header
-            ref={ref}
-            shadow={y > height ? "sm" : undefined}
-            bg={isScrolled ? "#000505" : "transparent"}
-            //add transition
-            transition="0.1s linear"
-            w="full"
-            sx={{
-                borderImage:
-                    "linear-gradient(90deg, rgba(1, 218, 124, 0) 2.69%, rgba(175, 174, 184, 0) 2.7%, rgba(175, 174, 184, 0.21) 50.12%, rgba(175, 174, 184, 0) 96.58%)",
-                borderImageSlice: 1,
-                borderBottomWidth: "1.5px",
-                borderBottomStyle: "solid",
-                borderBottomColor: "transparent",
-            }}
-            color="gray.800"
-            marginBottom={-90}
-            position="sticky"
-            top={0}
-            zIndex="10"
-        >
-            <chakra.div h="4.5rem" mx="auto" maxW="7xl" px={{ base: 2, sm: 4 }}>
-                <Flex
-                    w="full"
-                    h="full"
-                    px="6"
-                    align="center"
-                    justify="space-between"
-                >
-                    <Flex align="center">
-                        <Link href="/">
-                            <HStack w="full">
-                                <Text
-                                    fontSize="xl"
-                                    fontWeight="medium"
-                                    ml={5}
-                                    color="black"
-                                    _dark={{ color: "white" }}
-                                >
-                                    Stox
-                                </Text>
-                            </HStack>
-                        </Link>
-                    </Flex>
+    //if scrollY > 100, change navbar color
+    const [scrollNav, setScrollNav] = React.useState(false)
 
-                    <Box
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
-                        ml={20}
-                    >
+    const changeNav = () => {
+        if (window.scrollY >= 100) {
+            setScrollNav(true)
+        } else {
+            setScrollNav(false)
+        }
+    }
+
+    React.useEffect(() => {
+        window.addEventListener("scroll", changeNav)
+    }, [])
+
+    return (
+        <React.Fragment>
+            <chakra.header
+                // bg={bg}
+                position="fixed"
+                top={0}
+                w="full"
+                px={{
+                    base: 3,
+                    sm: 8,
+                }}
+                py={6}
+                zIndex="100"
+                transition="all 0.3s"
+                bg={scrollNav ? navBg : "transparent"}
+                shadow={scrollNav ? "md" : "none"}
+            >
+                <Flex
+                    alignItems="center"
+                    justifyContent="space-between"
+                    mx="auto"
+                    maxW="5xl"
+                >
+                    <Flex>
+                        <chakra.a
+                            href="/"
+                            title="Legal Dove"
+                            display="flex"
+                            alignItems="center"
+                        ></chakra.a>
+                        <chakra.h1
+                            fontSize="xl"
+                            fontWeight="900"
+                            ml="2"
+                            color="black"
+                            _dark={{
+                                color: "white",
+                            }}
+                            // fontFamily="Playfair Display"
+                        >
+                            Stox
+                        </chakra.h1>
+                        {/* <Image
+                            src={useColorModeValue("/logo.svg", "/logod.svg")}
+                            alt="Logo"
+                            width="200px"
+                            ml={12}
+                        /> */}
+                    </Flex>
+                    <HStack display="flex" alignItems="center" spacing={7}>
                         <HStack
-                            spacing="2"
+                            spacing={1}
+                            mr={1}
+                            color="brand.500"
                             display={{
                                 base: "none",
-                                lg: "flex",
+                                md: "inline-flex",
                             }}
+                        ></HStack>
+                        <Button
+                            size="large"
+                            variant="ghost"
+                            fontSize="2xl"
+                            _hover={{ bg: "" }}
                         >
-                            <Button
-                                alignItems="center"
-                                fontSize={{ base: "sm", md: "md" }}
-                                fontWeight="medium"
-                                _hover={{
-                                    color: cl,
-                                }}
-                                _focus={{
-                                    boxShadow: "none",
-                                }}
-                                variant="ghost"
-                            >
-                                Protocol
-                            </Button>
-                            <Button
-                                alignItems="center"
-                                fontSize={{ base: "sm", md: "md" }}
-                                fontWeight="medium"
-                                _hover={{
-                                    color: cl,
-                                }}
-                                _focus={{
-                                    boxShadow: "none",
-                                }}
-                                variant="ghost"
-                            >
-                                Governance
-                            </Button>
-                            <Button
-                                alignItems="center"
-                                fontSize={{ base: "sm", md: "md" }}
-                                fontWeight="medium"
-                                _hover={{
-                                    color: cl,
-                                }}
-                                _focus={{
-                                    boxShadow: "none",
-                                }}
-                                variant="ghost"
-                            >
-                                Docs
-                            </Button>
-                            <Button
-                                alignItems="center"
-                                fontSize={{ base: "sm", md: "md" }}
-                                fontWeight="medium"
-                                _hover={{
-                                    color: cl,
-                                }}
-                                _focus={{
-                                    boxShadow: "none",
-                                }}
-                                variant="ghost"
-                            >
-                                About
-                            </Button>
-
-                            <Button
-                                alignItems="center"
-                                fontSize={{ base: "sm", md: "md" }}
-                                fontWeight="medium"
-                                _hover={{
-                                    color: cl,
-                                }}
-                                _focus={{
-                                    boxShadow: "none",
-                                }}
-                                rightIcon={<IoIosArrowDown />}
-                                variant="ghost"
-                            >
-                                More
-                            </Button>
-                        </HStack>
-                    </Box>
-
-                    <HStack spacing="2">
-                        <HStack spacing="5">
-                            <HStack
-                                spacing="5"
-                                display={{
-                                    base: "none",
-                                    lg: "flex",
-                                }}
-                                color="black"
-                                _dark={{ color: "white" }}
-                            >
-                                <Icon as={FaDiscord} />
-                                <Icon as={FaTwitter} />
-                            </HStack>
-                            <Button
-                                variant="solid"
-                                size="sm"
-                                width="150px"
-                                bg="white"
-                                color="black"
-                                onClick={toggleColorMode}
-                            >
-                                Toggle{" "}
-                                {colorMode === "light" ? "Dark" : "Light"}
-                            </Button>
-                        </HStack>
+                            Help
+                        </Button>
+                        <IoLanguage fontSize="30px" />
                         <IconButton
-                            display={{
-                                base: "flex",
-                                lg: "none",
-                            }}
-                            aria-label="Open menu"
-                            fontSize="20px"
-                            size="sm"
-                            color="gray.800"
-                            _dark={{
-                                color: "inherit",
-                            }}
+                            size="lg"
+                            fontSize="lg"
                             variant="ghost"
                             icon={
-                                mobileNav.isOpen ? (
-                                    <FaTimes
-                                        transform="rotate(90deg)"
-                                        style={{
-                                            transition: "all 0.3s ease-in-out",
-                                        }}
-                                    />
-                                ) : (
-                                    <AiOutlineMenu />
-                                )
+                                colorMode === "light" ? <IoMoon /> : <IoSunny />
                             }
-                            onClick={() => mobileNav.onToggle()}
+                            aria-label={`Switch to ${
+                                colorMode === "light" ? "dark" : "light"
+                            } mode`}
+                            onClick={toggleColorMode}
                         />
+                        <Box
+                            display={{
+                                base: "inline-flex",
+                                md: "none",
+                            }}
+                        >
+                            <IconButton
+                                display={{
+                                    base: "flex",
+                                    md: "none",
+                                }}
+                                aria-label="Open menu"
+                                fontSize="20px"
+                                color="gray.800"
+                                _dark={{
+                                    color: "inherit",
+                                }}
+                                variant="ghost"
+                                // icon={<AiOutlineMenu />}
+                                onClick={mobileNav.onOpen}
+                            />
+
+                            <VStack
+                                pos="absolute"
+                                top={0}
+                                left={0}
+                                right={0}
+                                display={mobileNav.isOpen ? "flex" : "none"}
+                                flexDirection="column"
+                                p={2}
+                                pb={4}
+                                m={2}
+                                bg={bg}
+                                spacing={3}
+                                rounded="sm"
+                                shadow="sm"
+                            >
+                                <CloseButton
+                                    aria-label="Close menu"
+                                    onClick={mobileNav.onClose}
+                                />
+                            </VStack>
+                        </Box>
                     </HStack>
                 </Flex>
-                {MobileNavContent}
-            </chakra.div>
-        </chakra.header>
+            </chakra.header>
+        </React.Fragment>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,40 @@
+import { useColorModeValue } from "@chakra-ui/react"
+import { motion } from "framer-motion"
+import type { NextPage } from "next"
 import Head from "next/head"
-import Image from "next/image"
-import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
 
-export default function Home() {
+const Home: NextPage = ({ data }: any) => {
+    console.log(data)
+    const bg = useColorModeValue("#fff", "#1A202C")
+
     return (
-        <>
+        // <motion.div
+        //     className="container text-center"
+        //     initial={{ opacity: 0 }}
+        //     animate={{ opacity: 1 }}
+        //     exit={{ opacity: 0 }}
+        //     transition={{ duration: 3 }}
+        // >
+        <div style={{ background: bg }}>
+            <Head>
+                <title>Stox</title>
+                <link rel="icon" href="/logo.svg"></link>
+            </Head>
             <Navbar />
-            <Hero />
-        </>
+            {/* <Hero data={data} /> */}
+        </div>
+        // </motion.div>
     )
+}
+
+export default Home
+
+export async function getServerSideProps() {
+    const res = await fetch("https://legaldove.geethg.com/api/data")
+    const data = await res.json()
+
+    return {
+        props: { data: data }, // will be passed to the page component as props
+    }
 }
